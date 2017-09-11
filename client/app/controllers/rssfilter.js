5971a5f730ab79984a1aa445903b462111dc589d
Jose Alvarez <josealvarez712@gmail.com>
2017-09-11 18:24:46 -0500

    modal added and detail page new fix

--- client/app/controllers/rssfilter.js
+++ client/app/controllers/rssfilter.js
@@ -7,19 +7,23 @@ export default Ember.Controller.extend({
     this.set('filter', "");
     this.set('result', []);
   },
-  filter: "Uniandes",
+  filter: "",
   result: [],
   filteredNews: Ember.computed('filter', function() {
-    this.set('result', []);
-    var self = this;
-    Ember.$.getJSON(
-      ENV.APP.API_HOST + '/api/v1/gnews/' + this.get('filter'),
-      function(data){
-        data.forEach(function(newFiltered){
-          self.result.addObject(newFiltered);
-        });
-      }
-    );
+    if(this.get('filter')!="" && this.get('filter')!=null){
+      this.set('result', []);
+      var self = this;
+      $('#modal').modal({backdrop: 'static', keyboard: false});
+      Ember.$.getJSON(
+        ENV.APP.API_HOST + '/api/v1/gnews/' + this.get('filter'),
+        function(data){
+          data.forEach(function(newFiltered){
+            self.result.addObject(newFiltered);
+          });
+          $('#modal').modal('hide');
+        }
+      );
+    }
   }),
   actions: {
     changeFilter(){
