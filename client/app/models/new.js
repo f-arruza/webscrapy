fce80507ddb14104917b740ce3dfd78574b3a468
Jose Alvarez <josealvarez712@gmail.com>
2017-09-11 01:37:05 -0500

    news fixed template for academic_unit field added

--- client/app/models/new.js
+++ client/app/models/new.js
@@ -4,7 +4,5 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   link: DS.attr('string'),
-  pubdate: DS.attr('string'),
-  category: DS.attr('string'),
-  source: DS.attr('string')
+  academic_unit: DS.attr('string')  
 });
